a2bbe3d3589e6fd13117e68ca8703268725285ee
flexabler <smolnikov2023@list.ru>
2026-01-28 23:16:46 +0500

    fix progression error

--- brain_games/games/brain_progression_game_logic.py
+++ brain_games/games/brain_progression_game_logic.py
@@ -7,7 +7,7 @@ def create_progression():
     result_progression = []
     step_progression = randint(1, 10)
     start_progression = randint(1, 10)
-    for i in range(1, randint(5, 11)):
+    for i in range(1, randint(6, 11)):
         result_progression.append(start_progression + i * step_progression)
 
     return result_progression
